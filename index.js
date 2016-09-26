94ce0654ee06d7e2c080d9c9c45f6cd4690f45c7
Ksenia Kaganer <ksenia.kaganer@gmail.com>
2016-09-26 16:53:12 +0300

    new_clone

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = 'mongodb://heroku_fg5vgt5n:g0p04ve08p4q6u1ujitvpgtosr@ds041516.mlab.com:41516/heroku_fg5vgt5n';
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
 
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || '4zfcA14POYukONe5ErKE6W2MBk5qCtr6eTGN8mDh',
-  masterKey: process.env.MASTER_KEY || '2qdHIXoVVr6JKPmZdKdNRneZgTO5jGAbuiyLZzxh', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://finaltestmig.herokuapp.com/',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
